a1a70994662284116084cb465cbc0753b57e0f97
Amiel David Del Pilar <amiel.david.del.pilar@oocl.com>
2022-09-01 14:14:24 +0800

    Test: Created test case for return number 1 when count off

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_true_when_count_off_given_number_1() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "1";
+
+        //when
+        String actual = fizzBuzz.countOff(1);
+
+        //then
+        assertEquals(expected, actual);
+    }
 }
