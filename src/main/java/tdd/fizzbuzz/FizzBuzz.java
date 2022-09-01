d092b31386339063c757a78cacbf135ed8a34894
Amiel David Del Pilar <amiel.david.del.pilar@oocl.com>
2022-09-01 14:27:28 +0800

    Feat: Added logic to return Fizz if input is divisible by 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,12 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    private static final String FIZZ = "Fizz";
+
     public String countOff(int order) {
+        if(order % 3 == 0) {
+            return FIZZ;
+        }
         return Integer.toString(order);
     }
 }
