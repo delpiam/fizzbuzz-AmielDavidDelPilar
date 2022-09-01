349cd89a64e3ee25d781a3a9bda2ee330999cef9
Amiel David Del Pilar <amiel.david.del.pilar@oocl.com>
2022-09-01 14:47:24 +0800

    Feat: Added logic to return Whizz when input is divisible by 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,7 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
+    private static final String WHIZZ = "Whizz";
 
     public String countOff(int order) {
         if(order % 3 == 0) {
@@ -10,6 +11,9 @@ public class FizzBuzz {
         }
         if(order % 5 == 0) {
             return BUZZ;
+        }
+        if(order % 7 == 0) {
+            return WHIZZ;
         }
         return Integer.toString(order);
     }
